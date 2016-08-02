c46e1239815a2bfefe293c9af51fbe689fd58f0f
thelastpenguin <thelastpenguin212@gmail.com>
2016-08-01 22:19:40 -0700

    Update pON-developmental.lua

--- pON/pON-developmental.lua
+++ pON/pON-developmental.lua
@@ -110,7 +110,7 @@ do
       local tk, tv = type(k), type(v)
 
       -- WRITE KEY
-      if tk == 'strnig' then
+      if tk == 'string' then
         local pid = cache[ k ];
         if( pid )then
           output[ #output + 1 ] = format('(%x)',  pid );
